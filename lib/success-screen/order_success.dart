b60a681cd75f4b88f7010dd75c99ca7da57afeda
MohamedSalem <engsalem500@gmail.com>
2025-05-10 20:43:54 +0300

    SuccessOrderPage

--- lib/success-screen/order_success.dart
+++ lib/success-screen/order_success.dart
@@ -7,7 +7,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
 
 class SuccessOrderPage extends StatelessWidget {
-  const SuccessOrderPage({super.key});Success_Order_Page
+  const SuccessOrderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
